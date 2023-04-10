b3aa5c3607ed20cf65057670504bd3d45aeee6a4
Don <dagem.alemseged1@gmail.com>
2023-04-10 10:19:42 +0300

    Create sign up component, form input component, button component

--- dripvault/src/routes/sign-in/sign-in.component.jsx
+++ dripvault/src/routes/sign-in/sign-in.component.jsx
@@ -1,6 +1,25 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
+import { useEffect } from 'react'
+import { getRedirectResult } from 'firebase/auth'
+import { 
+    auth,
+    signInWithGooglePopup, 
+    signInWithGoogleRedirect, 
+    createUserDocumentFromAuth
+} from '../../utils/firebase/firebase.utils'
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component'
 
 const SignIn = () => {
+    
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await getRedirectResult(auth)
+            if (response) {
+                const userDocRef = await createUserDocumentFromAuth(response.user)
+            }
+        }
+        fetchData()
+    }, [])
+
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup()
         const userDocRef = await createUserDocumentFromAuth(user)
@@ -9,7 +28,9 @@ const SignIn = () => {
     return (
         <div>
             <h1>Sign In page</h1>
-            <button onClick={logGoogleUser}>Log in with Google</button>
+            <button onClick={logGoogleUser}>Sign in with Google</button>
+            {/* <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button> */}
+            <SignUpForm />
         </div>
     )
 }
