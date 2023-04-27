fe6a2f7890b8fe86ffd138773211cc8bab58bac4
Don <dagem.alemseged1@gmail.com>
2023-04-27 14:20:40 +0300

    Add in addToCart feature

--- dripvault/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ dripvault/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,23 @@
+import { useContext } from 'react'
+
+import { CartContext } from '../../contexts/cart.context.jsx'
+
 import Button from '../button/button.component.jsx'
+import CartItem from '../cart-item/cart-item.component.jsx'
 
 import './cart-dropdown.styles.scss'
 
 const CartDropdown = () => {
+    const { cartItems, cartCount } = useContext(CartContext)
+
     return (
         <div className = 'cart-dropdown-container'>
-            <div className = 'cart-items'></div>
+            <div className = 'cart-items'>
+                {
+                    cartCount === 0 ? 'Your cart is empty' : cartItems.map (item => (
+                        <CartItem key = {item.id} cartItem = {item} />
+                ))}
+            </div>
             <Button>GO TO CHECKOUT</Button>
         </div>
     )
