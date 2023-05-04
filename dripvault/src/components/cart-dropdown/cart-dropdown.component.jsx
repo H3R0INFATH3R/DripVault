32c583d90f93a54850f246b3b9225121537ea111
Don <dagem.alemseged1@gmail.com>
2023-05-04 12:48:40 +0300

    Create checkout page and checkout cart functionality

--- dripvault/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ dripvault/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,6 +2,8 @@ import { useContext } from 'react'
 
 import { CartContext } from '../../contexts/cart.context.jsx'
 
+import { useNavigate } from 'react-router-dom'
+
 import Button from '../button/button.component.jsx'
 import CartItem from '../cart-item/cart-item.component.jsx'
 
@@ -10,15 +12,20 @@ import './cart-dropdown.styles.scss'
 const CartDropdown = () => {
     const { cartItems, cartCount } = useContext(CartContext)
 
+    const navigate = useNavigate()
+    const goToCheckoutHandler = () => {
+        navigate('/checkout')
+    }
+
     return (
         <div className = 'cart-dropdown-container'>
             <div className = 'cart-items'>
                 {
                     cartCount === 0 ? 'Your cart is empty' : cartItems.map (item => (
-                        <CartItem key = {item.id} cartItem = {item} />
+                        <CartItem key = { item.id } cartItem = { item } />
                 ))}
             </div>
-            <Button>GO TO CHECKOUT</Button>
+            <Button onClick = { goToCheckoutHandler }>GO TO CHECKOUT</Button>
         </div>
     )
 }
